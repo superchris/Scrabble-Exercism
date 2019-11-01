9edfa989d7364e3e67191a25013cc379ce4487a3
Chris Nelson <superchrisnelson@gmail.com>
2019-11-01 16:22:52 -0400

    datastructure magic

--- ScrabbleScore.cs
+++ ScrabbleScore.cs
@@ -4,8 +4,8 @@ using System.Linq;
 
 public static class ScrabbleScore
 {
-    static Dictionary<int, string> values = new Dictionary<int, string>()
-        
+    static Dictionary<char, int> values = new Dictionary<int, string>()
+
         {
             {1, "aeioulnrst"},
             {2, "dg"},
@@ -14,13 +14,16 @@ public static class ScrabbleScore
             {5, "k"},
             {8, "jx"},
             {10, "qz"}
-      };
-    private static int letterValues(char letter)
-    {   
-        int value = values.FirstOrDefault(x => x.Value.Contains(letter)).Key;
+      }.SelectMany(kv => kv.Value.Select(c => (c, kv.Key)))
+      .ToDictionary(kv => kv.c, kv => kv.Key);
 
-        return value;
-    } 
+    private static int letterValues(char letter)
+    {
+      return values.GetValueOrDefault(letter);
+        // int value = values.FirstOrDefault(x => x.Value.Contains(letter)).Key;
+
+        // return value;
+    }
     public static int Score(string input)
     {
         int score = 0;
@@ -29,22 +32,22 @@ public static class ScrabbleScore
         var scores = word.Select(c => letterValues(c));
 
         score = scores.Sum();
-        
+
         return score;
     }
-    public static int SpecialLetterScore(int score, string type, char letter) 
+    public static int SpecialLetterScore(int score, string type, char letter)
     {
         type = type.ToLower();
 
         int finalScore = type == "double" ? score + letterValues(letter) : type == "triple" ? score + (2 * letterValues(letter)): score;
 
         return finalScore;
-        
+
     }
-    public static int SpecialWordScore(int score, string type) 
+    public static int SpecialWordScore(int score, string type)
     {
         type = type.ToLower();
-        
+
         int finalScore = type == "double" ? score * 2 : type == "triple" ? score * 3 : score;
 
         return finalScore;
